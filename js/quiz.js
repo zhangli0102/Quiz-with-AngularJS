7b3e27082e28975386699f3070bbd76466ee1bc8
Li Zhang <zhangli@gwu.edu>
2015-11-23 14:50:46 -0500

    setting states

--- js/quiz.js
+++ js/quiz.js
@@ -13,6 +13,31 @@
             $scope.myQuestions = quizData.data;
             $scope.totalQuestions = $scope.myQuestions.length;
         });
+        
+        $scope.selectAnswer = function(qIndex, aIndex){
+            var questionState = $scope.myQuestions[qIndex].questionState;
+            
+            if( questionState != 'answered' ){
+                $scope.myQuestions[qIndex].selectedAnswer = aIndex;
+                var correctAnswer = $scope.myQuestions[qIndex].correct;
+                $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
+                
+                if ( aIndex === correctAnswer ){
+                    $scope.myQuestions[qIndex].correctness = 'correct';
+                    $scope.score += 1;
+                }else {
+                    $scope.myQuestions[qIndex].correctness = 'incorrect';
+                }
+                $scope.myQuestions[qIndex].questionState = 'answered';
+            }
+        }
+        
+        $scope.isSelected = function(qIndex,aIndex){
+            return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+        }
+        $scope.isCorrect = function(qIndex,aIndex){
+            return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+        }
     
     }]);
 
