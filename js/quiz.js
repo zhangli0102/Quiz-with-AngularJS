e19755905790b6d9baca869f7fc1617be4c38bf6
Li Zhang <zhangli@gwu.edu>
2015-11-22 23:09:16 -0500

    setting ng
    app and controller

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,11 @@
 (function(){
 
-	// script goes here
+    var app = angular.module('myQuiz',[]);
+    
+    app.controller('QuizController',['$scope','$http','$sce',function($scope,$http,$sce){
+    
+        
+    
+    }]);
 
 })();
