955d9be93d0b68b589ae79662a25ae9b0c3c2164
Li Zhang <zhangli@gwu.edu>
2015-11-22 23:43:26 -0500

    loading json

--- js/quiz.js
+++ js/quiz.js
@@ -4,7 +4,15 @@
     
     app.controller('QuizController',['$scope','$http','$sce',function($scope,$http,$sce){
     
+        $scope.score = 0;
+        $scope.activeQuestion = -1;
+        $scope.activeQuestionAnswered = 0;
+        $scope.percentage = 0;
         
+        $http.get('quiz_data.json').then(function(quizData){
+            $scope.myQuestions = quizData.data;
+            $scope.totalQuestions = $scope.myQuestions.length;
+        });
     
     }]);
 
